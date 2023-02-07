8e6037158fbf5c74733755bc96e8c5847b09fe65
swmac <shaynefw@gmail.com>
2023-02-07 17:49:38 -0500

    added question prompts

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+inquirer.prompt([
+    {
+        name: 'key',
+        type: 'press-to-continue',
+        message: 'Press any key to continue...'
+    },
+    {
+        name: 'Title',
+        type: 'input',
+        message: 'What is the title of your repo?'
+    },
+    {
+        name: 'Description',
+        type: 'input',
+        message: 'Provide a short description.'
+    },
+    {
+        name: 'Installation',
+        type: 'input',
+        message: 'What are the steps required to install your project?'
+    }
+])
+.then((response) => {
+    fs.writeFile('log.txt', JSON.stringify(response), (err) => {
+        err ? console.error(err) : console.log('File created!');
+    });
+});
+
+
+
+// console.log(pressToContinue);
+// console.log(questions)
